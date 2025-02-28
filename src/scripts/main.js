c72689f3fc7727f39e61778fbc61e32d56ffc683
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-28 17:18:14 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+appendRow.addEventListener('click', (e) => {
+  const copyOfRow = document.querySelector('tr').cloneNode(true);
+  const parentElement = document.querySelector('tbody');
+
+  if (parentElement.children.length < 10) {
+    parentElement.append(copyOfRow);
+  }
+});
+
+removeRow.addEventListener('click', (e) => {
+  const parentElement = document.querySelector('tbody');
+
+  if (parentElement.children.length > 2) {
+    parentElement.lastElementChild.remove();
+  }
+});
+
+appendColumn.addEventListener('click', (e) => {
+  const allRows = Array.from(document.querySelectorAll('tr'));
+  const checkLength = document.querySelector('tr').children.length;
+
+  if (checkLength < 10) {
+    for (const row of allRows) {
+      const copyOfColumn = document.querySelector('td').cloneNode(true);
+
+      row.append(copyOfColumn);
+    }
+  }
+});
+
+removeColumn.addEventListener('click', (e) => {
+  const allRows = Array.from(document.querySelectorAll('tr'));
+  const checkLength = document.querySelector('tr').children.length;
+
+  if (checkLength > 2) {
+    for (const row of allRows) {
+      row.lastElementChild.remove();
+    }
+  }
+})
